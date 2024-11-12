a1b116247ea73f798b1a5deb4615034320af293d
sfkim <samson.chang2@gmail.com>
2024-11-11 22:26:40 -0800

    first commit

--- convert.py
+++ convert.py
@@ -1,1 +1,9 @@
+from typing import Optional
 
+'''This function takes a string value and sees if that value can become a float by using the float() function.
+ If it can't, and gets a value error, it returns None'''
+def string_to_float(values:str)->Optional[float]:
+    try:
+       return float(values)
+    except ValueError:
+        return None
--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,18 @@
+from asyncio import gather
 
+from convert import string_to_float
+
+'''This function takes no parameters because it gets the input from the user and sees if the value is a float using the 
+defined function string_to_float in convert.py. If the input from the user is indeed a float, it appends it to user_list 
+and returns it to the console'''
+def gather_numbers()->list[float]:
+    user_list = []
+    for i in range(10):
+        user_input = input('Enter a number!')
+        if string_to_float(user_input) is not None:
+            user_list.append(float(user_input))
+    print( user_list)
+    return user_list
+
+if __name__ == '__main__':
+    print(sum(gather_numbers()))
